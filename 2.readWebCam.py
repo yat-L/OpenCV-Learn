ddefa4a29ad8c2b2aecf9817a784a522e90de1a3
Yat <ylui626@gmail.com>
2022-05-20 14:06:51 -0500

    added blur

--- 2.readWebCam.py
+++ 2.readWebCam.py
@@ -1,5 +1,6 @@
 import cv2
 
+
 def main():
     webCam = cv2.VideoCapture(0)
 
@@ -11,18 +12,28 @@ def main():
         frameCount = webCam.get(cv2.CAP_PROP_FRAME_COUNT)
         print(f'Fame Count is: {frameCount}')
 
+
     while webCam.isOpened():
         available, frame = webCam.read()
         if available:
-            sk_gray, sk_color = cv2.pencilSketch(frame, sigma_s=60, sigma_r=0.07, shade_factor=0.1) # pencil effect
-            result = cv2.bitwise_not(sk_gray) # invert the color
-            cv2.imshow('Frame',result)
-            quitKey = cv2.waitKey(1)
+            result = convertAndShow(frame, 60, 0.07, 0.1, 5, 0)
+
+            quitKey = cv2.pollKey()
             if quitKey == ord('q'):  # break when pressed 'q'
+                break
+            elif quitKey == ord('s'):
+                cv2.imwrite('screenShot.jpg',result)
                 break
         else:
                 break
 
+
+def convertAndShow(frame, sigmaS, sigmaR, shade, blurKer, SigmaXY):
+            frame = cv2.GaussianBlur(frame, (blurKer,blurKer), SigmaXY)
+            sk_gray, sk_color = cv2.pencilSketch(frame, sigma_s=sigmaS, sigma_r=sigmaR, shade_factor=shade) # pencil effect
+            result = cv2.bitwise_not(sk_gray) # invert the color
+            cv2.imshow('Frame',result)
+            return result
 
 
 
