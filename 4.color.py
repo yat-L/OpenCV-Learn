48c65851f73e3c3f7b7f2a7b5011441f065b7ff2
Yat <ylui626@gmail.com>
2022-05-28 22:56:43 -0500

    h stack

--- 4.color.py
+++ 4.color.py
@@ -1,9 +1,15 @@
 import cv2 as cv
+import numpy as np
 
 rubik = cv.imread("data/rubiks.jpg")
 rubikLAB = cv.cvtColor(rubik,cv.COLOR_BGR2LAB)
 (L,A,B) = cv.split(rubikLAB)
-cv.imshow('frame1',L)
-cv.imshow('frame2',A)
-cv.imshow('frame3',B)
+L_H = A_H = B_H = None
+L_H = cv.resize(L,(0,0),None,0.35,0.35)
+A_H = cv.resize(A,(0,0),None,0.35,0.35)
+B_H = cv.resize(B,(0,0),None,0.35,0.35)
+
+numpyHstack = np.vstack((L_H,A_H,B_H))
+
+cv.imshow('LAB Color',numpyHstack)
 cv.waitKey(0)
